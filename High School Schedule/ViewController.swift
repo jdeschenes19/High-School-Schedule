be2e04515ad27cf6fbb0fb589d1eb7721fca4b59
jdeschenes19 <jdeschenes19@students.polytechnic.org>
2015-05-27 11:13:04 -0700

    Starting

---
+++ High School Schedule/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  High School Schedule
+//
+//  Created by JAM2 on 5/27/15.
+//  Copyright (c) 2015 JAM2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
